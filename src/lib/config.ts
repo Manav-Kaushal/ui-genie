68ab23bd73429150a8200d9e137344bb7a4c98db
Manav Kaushal <manavkaushal756@gmail.com>
2026-01-04 02:12:36 +0530

    added config file

---
+++ src/lib/config.ts
@@ -0,0 +1,14 @@
+export const appConfig = {
+  name: "ui-genie",
+  displayName: "ui genie",
+  author: "Manav Kaushal",
+  company: "",
+  social: {
+    github: "",
+    twitter: "",
+    linkedin: "",
+    youtube: "",
+    instagram: "",
+    facebook: "",
+  },
+};
