9616b2f92ea5003acc866958648b0b6ff4fb3976
Manav Kaushal <manavkaushal756@gmail.com>
2026-01-04 16:27:59 +0530

    schemas updated

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,5 +1,5 @@
 import Google from "@auth/core/providers/google";
-import { Password } from "@convex-dev/auth/providers/password";
+import { Password } from "@convex-dev/auth/providers/Password";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
