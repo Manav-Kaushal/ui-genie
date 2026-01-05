9d7491eb4f52924377ddcc5c9caec8fa66e10804
Manav Kaushal <manavkaushal756@gmail.com>
2026-01-06 01:19:17 +0530

    integrated redux and created a user profile's store

---
+++ src/redux/provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { makeStore, RootState } from "./store";
+
+type ReduxProviderProps = {
+  children: React.ReactNode;
+  preloadedState?: Partial<RootState>;
+};
+
+const ReduxProvider = ({ children, preloadedState }: ReduxProviderProps) => {
+  const storeRef = useRef(makeStore(preloadedState));
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
+};
+
+export default ReduxProvider;
